62544a7bbf12519137ea3ced0941b647f7f6ef64
Runar Buvik <rb@searchdaimon.com>
2015-08-03 20:59:07 +0200

    Added a quick report option

---
+++ src/ProxyMaid/FormReport.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ProxyMaid
+{
+    partial class FormReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxReport = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxReport
+            // 
+            this.textBoxReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxReport.Location = new System.Drawing.Point(0, 0);
+            this.textBoxReport.Multiline = true;
+            this.textBoxReport.Name = "textBoxReport";
+            this.textBoxReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReport.Size = new System.Drawing.Size(548, 336);
+            this.textBoxReport.TabIndex = 0;
+            // 
+            // FormReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 336);
+            this.Controls.Add(this.textBoxReport);
+            this.Name = "FormReport";
+            this.Text = "Report";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxReport;
+    }
+}
---
+++ src/ProxyMaid/FormReport.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProxyMaid
+{
+    public partial class FormReport : Form
+    {
+        public FormReport(Globals Global)
+        {
+            InitializeComponent();
+
+            textBoxReport.AppendText("# Checked with ProxyMaid ( http://www.proxymaid.com/ )\n");
+            textBoxReport.AppendText("# Time out: " + Properties.Settings.Default.ProxyTimeOut + "s\n");
+            
+
+            string[] anonymitylevels = { "High", "Low", "None" };
+            foreach (string anonymity in anonymitylevels)
+            {
+                textBoxReport.AppendText("#\n");
+                textBoxReport.AppendText("# Proxies with " + anonymity.ToLower() + " anonymity:\n");
+
+                foreach (ProxyServer server in Global.ProxyServers.ToList())
+                {
+                    if (server.Status.Substring(0, 2) == "Ok" && server.Anonymity == anonymity)
+                    {
+                        textBoxReport.AppendText(server.Ip + ":" + server.Port + "\n");
+                    }
+                }
+            }
+        }
+    }
+}
