33e409160bbd5831d286f4543922d036dd0301d0
Runar Buvik <rb@searchdaimon.com>
2015-08-28 23:14:30 +0200

    Added description.

--- src/ProxyMaid/Properties/AssemblyInfo.cs
+++ src/ProxyMaid/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProxyMaid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An open source proxy checker.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ProxyMaid")]
