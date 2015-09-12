d6abbea0ef27c01e1d014b54cc38be834b8f4889
Runar Buvik <rb@searchdaimon.com>
2015-09-12 14:43:40 +0200

    Set timeout value for phantomjs,
    Prevention slow pages to hang the application.

--- src/ProxyMaid/JavaScript/plainText.js
+++ src/ProxyMaid/JavaScript/plainText.js
@@ -3,7 +3,16 @@ var webPage = require('webpage');
 var page = require('webpage').create(),
     system = require('system'),
     url;
-	
+
+// Timeout: Prevention slow pages to hang the application.
+page.settings.resourceTimeout = 60000; // 60 seconds
+page.onResourceTimeout = function(e) {
+  console.log(e.errorCode);
+  console.log(e.errorString);
+  console.log(e.url);
+  phantom.exit(1);
+};
+    
 if (system.args.length != 2) {
     console.log('Usage: plainText.js URL');
     phantom.exit(1);
